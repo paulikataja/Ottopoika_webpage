5e630f6dd305fb8266b4d854933e589b8dd991b8
paulikataja <60697870+paulikataja@users.noreply.github.com>
2020-03-30 17:28:47 +0300

    poistettu pari kommenttirivia

--- contact.php
+++ contact.php
@@ -3,49 +3,7 @@
 	
 	include('header.php');
 	
-	/*
-		NOTE:
-		In the form in contact.php, the name text field has the name "name"
-		If the user submits the form, the $_POST['name'] variable will be
-		automatically created, and will contain the text they typed into
-		the field. The $_POST['email'] variable will contain whatever they typed
-		into the email field.
 	
-	
-		PHP used in this script:
-		
-		preg_match()
-		- Perform a regular expression match
-		- http://ca2.php.net/preg_match
-		
-		isset()
-		- Determine if a variable is set and is not NULL
-		- http://ca2.php.net/manual/en/function.isset.php
-		
-		$_POST
-		- An associative array of variables passed to the current script via the HTTP POST method.
-		- http://www.php.net/manual/en/reserved.variables.post.php
-		
-		trim()
-		- Strip whitespace (or other characters) from the beginning and end of a string
-		- http://www.php.net/manual/en/function.trim.php
-		
-		exit
-		- Output a message and terminate the current script
-		- http://www.php.net//manual/en/function.exit.php
-		
-		die()
-		- Equivalent to exit
-		- http://ca1.php.net/manual/en/function.die.php
-		
-		wordwrap()
-		- Wraps a string to a given number of characters
-		- http://ca1.php.net/manual/en/function.wordwrap.php
-		
-		mail()
-		- Send mail
-		- http://ca1.php.net/manual/en/function.mail.php
-	*/
 	
 ?>
 
